04f19ad00acdafacf0548a7ce894eaa462ef49e3
Douglas <doug.neo@gmail.com>
2020-12-07 16:24:13 -0300

    Gerada e configurada as classes Pessoa Fisica e Juridica.

---
+++ S10V137 Impostos/Entities/PessoaFisica.cs
@@ -0,0 +1,21 @@
+﻿namespace S10V137_Impostos.Entities
+{
+    class PessoaFisica : Contribuinte
+    {
+        public double GastoSaude { get; set; }
+
+        public PessoaFisica(string name, double rendaAnual, double gastoSaude)
+            : base(name, rendaAnual)
+        {
+            GastoSaude = gastoSaude;
+        }
+
+        public override double Imposto()
+        {
+            if (RendaAnual < 20000)
+                return (RendaAnual * 0.15) - (GastoSaude * 0.5);
+            else
+                return (RendaAnual * 0.25) - (GastoSaude * 0.5);
+        }
+    }
+}
---
+++ S10V137 Impostos/Entities/PessoaJuridica.cs
@@ -0,0 +1,21 @@
+﻿namespace S10V137_Impostos.Entities
+{
+    class PessoaJuridica : Contribuinte
+    {
+        public int Funcionarios { get; set; }
+
+        public PessoaJuridica(string name, double rendaAnual, int funcionarios)
+            : base(name, rendaAnual)
+        {
+            Funcionarios = funcionarios;
+        }
+
+        public override double Imposto()
+        {
+            if (Funcionarios > 10)
+                return RendaAnual * 0.14;
+            else
+                return RendaAnual * 0.16;
+        }
+    }
+}
