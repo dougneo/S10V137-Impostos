89d18a9b33314ca95682d3b197157c4374af4b36
Douglas <doug.neo@gmail.com>
2020-12-07 16:00:24 -0300

    Gerada a Classe Abstrata Contribuinte.

---
+++ S10V137 Impostos/Entities/Contribuinte.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S10V137_Impostos.Entities
+{
+    abstract class Contribuinte
+    {
+        public string Name { get; set; }
+        public double RendaAnual { get; set; }
+
+        public Contribuinte(string name, double rendaAnual)
+        {
+            Name = name;
+            RendaAnual = rendaAnual;
+        }
+
+        public abstract double Imposto();
+    }
+}
